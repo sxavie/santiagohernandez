81c79b4d8dd50e435731f87672a23848a6e37b99
xaviierhdz <xaviierhdz@gmail.com>
2024-12-12 23:44:07 +0000

    site v2 commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://github.com/sxavie',
-  base: 'santiagohernandez',
+  integrations: [tailwind()]
 });
